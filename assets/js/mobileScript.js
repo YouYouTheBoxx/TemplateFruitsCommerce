e336e4704ea7424d40696f5ad6c565fc0a633255
Younes Benmeuraiem <younes.benmeuraiem7@gmail.com>
2022-02-14 14:01:07 +0100

    Nouvel version du JS

--- assets/js/mobileScript.js
+++ assets/js/mobileScript.js
@@ -11,23 +11,33 @@ for(let i=0; i < 5; i++) {
     let fruit = fruits["fruits"]["names"][i];
 
     let fruitSection = document.createElement('li');
-    fruitSection.innerHTML = "<a href=\"#\" class=\"nav-link link-dark mobileSidebarLink\" aria-current=\"page\" title=\"\"\n" +
-        "           data-bs-toggle=\"tooltip\" data-bs-placement=\"right\" data-bs-original-title=\"Banane\">\n" +
+    fruitSection.className = "nav-item";
+    fruitSection.innerHTML = "<a href=\"#\" class=\"nav-link\" aria-current=\"page\" title=\"\"\n" +
+        "           data-bs-toggle=\"tooltip\" data-bs-placement=\"right\" data-bs-original-title=\""+ fruit + "\">\n" +
         "            <img class=\"mobileSidebarImg\" alt=\"\" src=\"assets/img/fruits/" + fruit + ".png\">\n" +
         "        </a>";
     document.querySelector('.nav.nav-pills.flex-column.mb-auto').appendChild(fruitSection);
 }
 
 // Code pour la sidebar en version mobile --- Affichage de l'élément selectionné
-let navLinks = document.querySelectorAll('.mobileSidebarLink');
+let navLinks = document.querySelectorAll('.nav-link');
 
 navLinks.forEach( (element) => {
     element.addEventListener('click', (e) => {
-        let currentActive = document.querySelector('.mobileSidebarLink.active');
+        let currentActive = document.querySelector('.nav-link.active');
         if(currentActive !== null){
             currentActive.classList.remove('active');
         }
 
         element.classList.add('active');
     });
+});
+
+document.querySelector('#sideBar > #mobileBtn').addEventListener('click', (e) => {
+    let innerSidebar = document.querySelector('#ul-sidebar');
+    if(innerSidebar.style.transform === "translateX(0px)"){
+        innerSidebar.style.transform = "translateX(-100%)";
+    } else {
+        innerSidebar.style.transform = "translateX(0px)";
+    }
 });
